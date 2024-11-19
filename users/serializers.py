daee5f2957dbd9a2f5b8081c86367cd6197e99b6
Anas Kaarov <anasbinkurban@gmail.com>
2024-11-20 00:26:15 +0600

    refac: added business field to register

--- users/serializers.py
+++ users/serializers.py
@@ -13,22 +13,22 @@ from .models import User
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'phone_number']
+        fields = ["username", "phone_number"]
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        phone_number = attrs.get('phone_number')
-        password = attrs.get('password')
+        phone_number = attrs.get("phone_number")
+        password = attrs.get("password")
 
         user = authenticate(phone_number=phone_number, password=password)
         if not user:
-            raise AuthenticationFailed('Invalid phone number or password.')
+            raise AuthenticationFailed("Invalid phone number or password.")
 
         refresh = self.get_token(user)
         return {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
         }
 
 
@@ -36,7 +36,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
     phone_number = serializers.CharField(
         validators=[
             RegexValidator(
-                regex=r'^\+996\d{9}$',
+                regex=r"^\+996\d{9}$",
                 message="Phone number must be in the format '+996XXXXXXXXX'."
             )
         ]
@@ -45,29 +45,32 @@ class RegistrationSerializer(serializers.ModelSerializer):
         write_only=True,
         required=True,
         validators=[validate_password],
-        style={'input_type': 'password'}
+        style={"input_type": "password"}
     )
     confirm_password = serializers.CharField(
         write_only=True,
         required=True,
-        style={'input_type': 'password'}
+        style={"input_type": "password"}
     )
+    business = serializers.BooleanField(required=False, default=False)
 
     class Meta:
         model = User
-        fields = ['phone_number', 'password', 'confirm_password']
+        fields = ["phone_number", "password", "confirm_password", "business"]
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['confirm_password']:
+        if attrs["password"] != attrs["confirm_password"]:
             raise serializers.ValidationError({"password": "Passwords do not match."})
         return attrs
 
     def create(self, validated_data):
-        validated_data.pop('confirm_password')  # Remove confirm_password from validated data
+        validated_data.pop("confirm_password")
+        business = validated_data.pop('business', False)
         try:
             user = User.objects.create_user(
-                phone_number=validated_data['phone_number'],
-                password=validated_data['password']
+                phone_number=validated_data["phone_number"],
+                password=validated_data["password"],
+                business=business,
             )
             self.instance = user
             return user
